4ef199b55716c089371200259c2ff32935ba1a88
Akihito Koriyama <akihito.koriyama@gmail.com>
2013-06-11 01:22:57 +0900

    fix psr2

--- docs/editor_html/FooClass.php
+++ docs/editor_html/FooClass.php
@@ -1,9 +1,12 @@
 <?php
+
+namespace vendor;
+
 /**
  * This file is part of the BEAR.Ace package
  *
  * @license http://opensource.org/licenses/bsd-license.php BSD
- */ 
+ */
 class FooClass
 {
     public function get()
